006a2179599165863274e4c0f4140385a5382ad6
Shrvim <shrutika1351@gmail.com>
2023-03-08 00:55:37 -0500

    First Commit

---
+++ src/CoreJava/dataTypes.java
@@ -0,0 +1,21 @@
+package CoreJava;
+
+public class dataTypes {
+	
+	public static void main(String[] args) {
+		
+		int num = 5;
+		String a = "Hello World";
+		char letter = 'r';
+		double num1 = 4.5;
+		boolean s = true;
+		
+		System.out.println(num );
+		
+		
+		
+		
+		
+	}
+
+}
